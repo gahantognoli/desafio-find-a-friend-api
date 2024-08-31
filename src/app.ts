5b01866508defe4ccda5cd7f2181784e80135816
Gabriel Antognoli <gabriel_antognoli@hotmail.com>
2024-08-31 13:30:18 -0300

    Configuração inicial do projeto

---
+++ src/app.ts
@@ -0,0 +1,39 @@
+import fastify from 'fastify'
+import { ZodError } from 'zod'
+import { env } from './env'
+import fastifyJwt from '@fastify/jwt'
+import fastifyCookie from '@fastify/cookie'
+
+export const app = fastify()
+
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
+  cookie: {
+    cookieName: 'refresh-token',
+    signed: false,
+  },
+  sign: {
+    expiresIn: '10m',
+  },
+})
+
+app.register(fastifyCookie)
+
+app.setErrorHandler((error, _, reply) => {
+  if (error instanceof ZodError) {
+    return reply.status(400).send({
+      message: 'Validation error',
+      issues: error.format(),
+    })
+  }
+
+  if (env.NODE_ENV !== 'production') {
+    console.error(error)
+  } else {
+    // TODO: Aqui nós devemos fazer log pra um serviço externo
+  }
+
+  return reply.status(500).send({
+    message: 'Internal server error',
+  })
+})
